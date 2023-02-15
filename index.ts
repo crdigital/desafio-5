fe6fd844e1e4da3f9b440a2e01af3569506fa478
Clayton da Silva Rodrigues <clayton.metal2@hotmail.com>
2023-02-15 20:02:58 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
 
+const primeiroNome = "Clayton";
+const diaDoMes = 15;
+const hojeFezSol = true;
+
+type Cachorrro = {
+    nome: string;
+    idade: number;
+    raça: string;
+    grandePorte: boolean;
+    donos: string;
+  }
+  
+  interface Animais {
+    tipo: string;
+    bipede: boolean;
+    asas: boolean;
+    região: string;
+  }
+
+  type Gato = {
+    caracteristica: string;
+    cor?: string;
+  }
